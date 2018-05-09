ce36a5468fa61bee96604afe8d39a32e220e2e26
David MacKintosh <dgawmackintosh@gmail.com>
2018-05-09 13:46:53 +0100

    Adds name entry and cookie storage

--- app.js
+++ app.js
@@ -2,15 +2,22 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-	res.render('index');
+	const name = req.cookies.username;
+	if(name) {
+		res.render('index', { name });	
+	} else {
+		res.redirect('/hello');
+	}
 });
 
 app.get('/cards', (req, res) => {
@@ -21,11 +28,22 @@ app.get('/cards', (req, res) => {
 });
 
 app.get('/hello', (req, res) => {
-	res.render('hello');
+	const name = req.cookies.username;
+	if(name) {
+		res.redirect('/');	
+	} else {
+		res.render('hello');
+	}
 });
 
 app.post('/hello', (req, res) => {
-	res.render('hello', { name: req.body.username });
+	res.cookie('username', req.body.username);
+	res.redirect('/');
+});
+
+app.post('/goodbye', (req, res) => {
+	res.clearCookie('username', { path: '/' });
+	res.redirect('/hello');
 });
 
 app.listen(3000, () => {
