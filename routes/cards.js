2c63cbd2531118bfe707d7741e1b98210b467a11
David MacKintosh <dgawmackintosh@gmail.com>
2018-05-10 12:58:19 +0100

    Random card selection and some changes to routes

--- routes/cards.js
+++ routes/cards.js
@@ -4,15 +4,20 @@ const { data } = require('../data/flashcardData.json');
 const { cards } = data;
 
 router.get('/', (req, res) => {
-	
+	const randomId = Math.floor(Math.random() * (cards.length - 1));
+	res.redirect(`/cards/${randomId}`);
 });
 
 router.get('/:id', (req, res) => {
     const { side } = req.query;
     const { id } = req.params;
+	if(!side) {
+		res.redirect(`/cards/${id}?side=question`);
+	}
+	const name = req.cookies.username;
     const text = cards[id][side];
     const { hint } = cards[id];
-    const templateData = { id, text };
+    const templateData = { id, text, name };
     if(side === 'question') {
     	templateData.hint = hint;
     	templateData.sideToShow = 'answer';
